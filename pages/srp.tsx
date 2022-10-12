b04908f80f5119f3433b3946d42776194a7fa07f
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-10-12 19:57:45 +0330

    add script tag from next

--- pages/srp.tsx
+++ pages/srp.tsx
@@ -1,15 +1,11 @@
-import Head from 'next/head';
+import Script from 'next/script';
 import React from 'react';
 
 const srp = () => {
   return (
     <>
-      <Head>
-        <script
-          type="text/javascript"
-          src="https://mbmarin.datgate.com/content/themes/dh5-child-clean/reactapps/srp/index.srp.js"
-        ></script>
-      </Head>
+      <Script src="https://mbmarin.datgate.com/content/themes/dh5-child-clean/reactapps/srp/index.srp.js"></Script>
+
       <div id="root"></div>
     </>
   );
