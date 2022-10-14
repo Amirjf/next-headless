64dd5b4d9df249234555013f4e77bfd650759559
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-10-14 15:41:04 +0330

    fix srp link

--- pages/srp.tsx
+++ pages/srp.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const srp = () => {
   return (
     <>
-      <Script src="https://www.nissansalem.com/content/themes/dh5-child-clean//reactapps/srp/index.srp.js"></Script>
+      <Script src="https://mbmarin.datgate.com/content/themes/dh5-child-clean/reactapps/srp/index.srp.js"></Script>
       <div id="root"></div>
     </>
   );
