ba6cdbf2d22876e26024fa3f6d5db8d7ec28f258
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2022-10-14 15:30:19 +0330

    change address for srp

--- pages/srp.tsx
+++ pages/srp.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const srp = () => {
   return (
     <>
-      <Script src="https://stage55.datgate.com/content/reactpress/apps/next/srp/index.js"></Script>
+      <Script src="https://nissansalem.datgate.com/content/reactpress/apps/next/srp/index.js"></Script>
       <div id="root"></div>
     </>
   );
